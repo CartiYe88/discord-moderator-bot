9dfd9ace2753305177836ae87a0ba63b8cd5935e
Caden <cadenchahal@gmail.com>
2026-07-28 14:22:44 -0700

    Add bot login logic with discord.js client

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("clientReady", () => {
+  console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
